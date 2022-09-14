ecedf43e8db5a7e0e102ebd863aa8ca184e60fb1
渡邉　展夢 <hiro_mu0166@icloud.com>
2022-09-14 15:24:32 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+    // eslint-disable-next-line
+  const [incompleteTodos, setIncompleteTodos] = useState(['あああ', 'いいい'])
   return (
     <>
-      <div>ToDoアプリ</div>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="imcomplete-area">
+        <p className="title">未完了のTODO</p>
+          <ul>
+            {incompleteTodos.map((todo) => {
+              return (
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>完了</button>
+                  <button>削除</button>    
+                </div>
+              );   
+            })}
+          </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ううう</li>
+            <button>戻す</button>    
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
