6a3b0fbffd0313ec330abe997a0e0e7e95b10702
渡邉　展夢 <hiro_mu0166@icloud.com>
2022-09-26 04:00:52 +0000

    Update: InputTodo.jsx, styles.css sammary: 共通でないクラスをコンポーネント内に移行

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+const inputArea = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+}
+
 export const InputTodo = (props) => {
     const { newTodo, onChange, onClick } = props
     return (
-        <div className="input-area">
+        <div style={inputArea}>
           <input placeholder="TODOを入力" value={newTodo} onChange={onChange} />
           <button onClick={onClick} >追加</button>
         </div>
